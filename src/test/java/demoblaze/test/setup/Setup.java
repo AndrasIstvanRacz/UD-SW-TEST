c2f0a5d05a3d6b377226540ca2df962b533e8bca
Duszka Ákos Attila <43644528+duszka-akos-attila@users.noreply.github.com>
2021-05-04 13:05:15 +0200

    Update Setup.java

--- src/test/java/demoblaze/test/setup/Setup.java
+++ src/test/java/demoblaze/test/setup/Setup.java
@@ -1,17 +1,16 @@
+  
 package demoblaze.test.setup;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
-import io.cucumber.java.en.Given;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Setup {
 
-    protected static WebDriver driver ;
+    public static WebDriver driver;
 
     @Before
     public void setWebDriver() {
@@ -28,5 +27,4 @@ public class Setup {
     public void closeWebDriver(){
         driver.quit();
     }
-
 }
